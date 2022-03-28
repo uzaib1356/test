1a4d2a8234a44af95b123161c068bdeb37b5b13b
uzaib1356 <101474221+uzaib1356@users.noreply.github.com>
2022-03-28 11:30:47 -0700

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,49 @@
+void main() {
+  // var num = 5;
+  // var fac = 1;
+  // for (var i = num; i >= 1; i--) {
+  //    fac *= i ;
+  // }
+  //     print(fac);
+//   List<int> arr = [4, 6, 2, 4, 6, 4, 4, 4, 4];
+//   int n = arr.length;
+//   findMajoriy(arr, n);
+// }
+//question #02
+// void findMajoriy(List<int> arr, int n) {
+//   int maxCount = 0;
+//   int index = -1;
+//   for (int i = 0; i < n; i++) {
+//     int count = 0;
+//     for (int j = 0; j < n; j++) {
+//       if (arr[i] == arr[j]) {
+//         count++;
+//       }
+//     }
+//     if (count > maxCount) {
+//       maxCount = count;
+//       index = i;
+//     }
+//   }
+//   if (maxCount > n / 2)
+//     print(arr[index]);
+//   else
+//     print("No majority element");
+//question#03
+// int num = 1;
+//   for (int i = 0; i < 4; i++) {
+//     for(int j = 0;j<=i;j++)
+//     {
+//       print('${(num)} ');
+//     }
+//     num++;
+
+//   }
+//question#04
+// var myarr = [4, 2, 8, 5, 9, 10];
+//   var sumOfArr = 0;
+//   for (var i = 0; i < myarr.length; i++) {
+//     sumOfArr += myarr[i];
+//   }
+//   print(sumOfArr);
+}
